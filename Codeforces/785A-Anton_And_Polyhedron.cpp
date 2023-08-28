b8e6b0b672cc0da00daf344df246f0ad700a4396
Oval_Elephant35 <principalquantum30@gmail.com>
2023-08-28 12:38:19 +0530

    CF

--- Codeforces/785A-Anton_And_Polyhedron.cpp
+++ Codeforces/785A-Anton_And_Polyhedron.cpp
@@ -7,11 +7,11 @@ int main(){
     while(tt--){
         string s;
         cin >> s;
-        if(s[i]=='T')ans+=4;
-        if(s[i]=='C')ans+=6;
-        if(s[i]=='O')ans+=8;
-        if(s[i]=='D')ans+=12;
-        if(s[i]=='I')ans+=20;
+        if(s[0]=='T')ans+=4;
+        if(s[0]=='C')ans+=6;
+        if(s[0]=='O')ans+=8;
+        if(s[0]=='D')ans+=12;
+        if(s[0]=='I')ans+=20;
     }
     cout << ans;
     return 0;
