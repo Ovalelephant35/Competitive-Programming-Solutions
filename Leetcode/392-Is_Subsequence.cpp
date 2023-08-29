9e36c3e5836ba5b816dfcaca20afcf49d09921ee
Anurag Singh <principalquantum30@gmail.com>
2023-08-29 22:29:45 +0530

    LC

---
+++ Leetcode/392-Is_Subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int len = t.size();
+        int ans = s.size();
+        int tmp = 0;
+        for(int i=0 ; i<len ; i++){
+            if(s[tmp]==t[i]){
+                tmp++;
+            }
+        }
+        if(tmp==(ans))return true;
+        else return false;
+    }
+};
