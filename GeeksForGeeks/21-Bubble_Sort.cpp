c49db52e00319e85f324b9a8b3e4f477720917a8
Anurag Singh <principalquantum30@gmail.com>
2023-09-09 11:47:17 +0530

    GFG

---
+++ GeeksForGeeks/21-Bubble_Sort.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+    public:
+    //Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
+    {
+        for(int i=1 ; i< n ; i++){
+            int tmp = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j]>tmp){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1] = tmp;
+        }
+    }
+};
