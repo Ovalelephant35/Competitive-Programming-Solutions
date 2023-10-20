d037375acf5c9bf9a41e9c6263351680045aa9a5
Anurag Singh <principalquantum30@gmail.com>
2023-10-20 17:29:05 +0530

    GFG

--- GeeksForGeeks/8-Smallest_Positive_Number.cpp
+++ GeeksForGeeks/8-Smallest_Positive_Number.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
+using namespace std;
 class Solution
 {
     public:
     //Function to find the smallest positive number missing from the array.
     int missingNumber(int arr[], int n) {
-    sort(arr,arr+n);
+    //sort(arr,arr+n);
        int num=1;
        for(int i = 0 ; i<n ;i++){
            if(arr[i]==num && arr[i]>=1){
