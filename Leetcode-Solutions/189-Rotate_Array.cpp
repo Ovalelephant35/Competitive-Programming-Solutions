1a3a33afbc2d3a6e11d6548ac5c21b1f349ef04c
Anurag Singh <principalquantum30@gmail.com>
2024-02-22 23:59:23 +0530

    Two-Pointer

--- Leetcode-Solutions/189-Rotate_Array.cpp
+++ Leetcode-Solutions/189-Rotate_Array.cpp
@@ -1,16 +1,33 @@
+#include <vector>
+using namespace std;
 class Solution {
-    public void rotate(int[] nums, int k) {
-        int a = nums.length;
-        int[] ans = new int[a];
-        int j= 0 ;
-        k%=a;
-        for(int i=0 ; i<a ; i++){
-            j = i+k;
-            if(j>=a)j-=a;
-            ans[j]=nums[i];
-        }
-        for(int i=0 ; i<a ; i++){
-            nums[i]=ans[i];
-        }
+public:
+  void rotate(vector<int> &nums, int k) {
+    k = k % nums.size();
+    if (k == 0)
+      return;
+    else {
+      int i = 0;
+      int r = nums.size() - 1;
+      while (i < r) {
+        swap(nums[i], nums[r]);
+        i++;
+        r--;
+      }
+      i = 0;
+      int j = k - 1;
+      r = k;
+      int l = nums.size() - 1;
+      while (i < j) {
+        swap(nums[i], nums[j]);
+        i++;
+        j--;
+      }
+      while (r < l) {
+        swap(nums[r], nums[l]);
+        r++;
+        l--;
+      }
     }
-}
+  }
+};
